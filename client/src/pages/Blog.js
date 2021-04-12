5ec5d346f06dfdd457d85a83e924228198673403
Young Ji Kim <youjmik@gmail.com>
2021-04-12 17:40:47 -0400

    setting up Blog page

--- client/src/pages/Blog.js
+++ client/src/pages/Blog.js
@@ -1,20 +1,11 @@
 import React from "react";
-
+import Cards from "../components/Cards/Cards"
 function Blog() {
     return (
-      <div>
-          <h1>Blog Section</h1>
-          <h6>Lorem ipsum dolor sit amet, veniam salutatus ad vel, ei vix iudicabit dissentiet, fugit percipit nec ad. Minim affert eloquentiam te quo. Causae scribentur in nec. Pri eros senserit ut, dolor rationibus adipiscing ex ius, ne est viris nusquam theophrastus. Vim cu quod vocent consectetuer, ad qui invidunt forensibus. Cu eum falli aliquip, ut aeque constituto incorrupte ius, per in mentitum laboramus neglegentur. Cu omnis persecuti mea.
 
-Possim euripidis mel te. Soleat feugait intellegat vis ut, sumo dicunt cotidieque mea id, ut cibo soleat eos. Dolore accusata at vis, ei eum tation putant. Eam an solum maluisset persequeris, labore feugiat feugait ad his. Facete percipit eos ut, cu cetero elaboraret philosophia quo. Pro laudem patrioque ei.
-
-Melius minimum per ne, ius quaeque nusquam probatus id. Vix solum nominati adolescens ne, viris comprehensam ne has. Magna prima omnesque cu sea, ancillae invidunt assueverit ea usu. Ex minim accommodare qui. Numquam intellegat cu sed, eum exerci mediocrem sapientem at, ut usu affert postea. Eos ei legimus vituperata, qui ut posidonium assueverit instructior, minimum scaevola perfecto in vel.
-
-Cum ex homero dignissim, per eu causae latine fastidii. Et verear temporibus disputando quo, in vel dico delenit persequeris. Qui in invidunt intellegam, no esse recusabo philosophia nec. Nec ei postea accusamus, minimum appareat appellantur nec ea, affert delenit singulis his ne. Mel ne veritus concludaturque, alia democritum eloquentiam no eam.
-
-Qui hinc liberavisse ad, pro in atqui labores maiestatis. In omnes integre menandri eos, vim animal platonem te. Ut simul ocurreret has, menandri electram complectitur has cu, no dico quando usu. Ex vel feugait delectus persecuti, mucius commodo vis no.</h6>
-      </div>
-  
+            <Cards/> 
+            /*Add another Component that pushes Posted Blogs HERE */
+            
     );
   }
   
