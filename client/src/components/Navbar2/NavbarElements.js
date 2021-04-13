c14790a9565a3e8c3b12bfc4dc71990f0e210747
Young Ji Kim <youjmik@gmail.com>
2021-04-13 13:49:19 -0400

    leveled out router/scroll funcationality height

--- client/src/components/Navbar2/NavbarElements.js
+++ client/src/components/Navbar2/NavbarElements.js
@@ -4,11 +4,11 @@ import styled from 'styled-components';
 
 
 
-export const Navdiv = styled.nav `
+export const Navdiv = styled.nav`
 
 background: #000;
 // height: 80px;
-margin-top : -80px;
+// margin-top : -80px;
 display: flex;
 justify-content: center;
 padding: 0.5rem calc((100vw - 1000px) / 2);
@@ -16,7 +16,7 @@ z-index: 10;
 
 `;
 
-export const NavbarContainer = styled.div `
+export const NavbarContainer = styled.div`
 
 display: flex;
 justify-content: space-between;
@@ -26,7 +26,7 @@ padding : 0 24px;
 max-width: 1100px;
 `;
 
-export const NavLogo = styled(LinkR) `
+export const NavLogo = styled(LinkR)`
 
 color : #fff;
 justify-self : flex-start;
@@ -42,7 +42,7 @@ text-decoration: none;
 `;
 
 
-export const MobileIcon = styled.div `
+export const MobileIcon = styled.div`
 display: none;
 @media screen and (max-width: 768px){
   display: block;
@@ -56,42 +56,45 @@ display: none;
 }
 `
 
-export const NavMenu = styled.ul `
+export const NavMenu = styled.ul`
 display: flex;
 align-items: center;
 list-style : none;
 text-align: center;
 margin-right: -22px;
+margin-top: 0;
+margin-bottom:0;
 @media screen and (max-width: 768px){
   display: none;
 }
 
 `
- 
-export const NavItem = styled.li 
-`
-height: 40px;
+
+export const NavItem = styled.li
+  `
+// height: 40px;
 `
 
 export const NavLinks = styled(LinkS)`
 
 color: #fff;
-font-size: 20px;
-display: flex;
-align-item: center;
-text-decoration: none ;
-padding :0 1rem;
-height: 100%
-cursor: pointer;
-$.active {
-  border-bottom: 3px solid #01bf71
-}
+    font-size: 16px;
+    display: flex;
+    align-items: center;
+    text-decoration: none ;
+    padding :0 1rem;
+    height: 100%
+    cursor: pointer;
+
+    $:active {
+      border-bottom: 3px solid #01bf71
+    }
 
 `
 
-export const NavBtn = styled.nav `
+export const NavBtn = styled.nav`
 display: flex;
-algin-items: center ;
+align-items: center ;
 
 @media screen and (max-width : 768px){
   display: none ;
