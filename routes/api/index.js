28e1cd0cf619ebbea27ba0347c551e588a9ffbd1
Rachel Wanke <rkhorne09@gmail.com>
2021-04-14 16:15:00 -0400

    merging

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const blogRoutes = require("./blog");
-const dashboardRoutes= require("./dashboard");
+const dashboardRoutes= require("../dashboard");
 const userRoutes = require("./user")
 
 // blog,dashboard, user routes
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const routes = require("./routes/dashboard")
 const app = express();
 
 
@@ -28,7 +28,13 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 // Add routes, both API and view
-app.use(routes);
+ //app.use(routes);
+// require("./routes/dashboard")(app)
+// require("./routes/blog")(app)
+// require("./routes/user")(app)
+// app.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/mindflo");
