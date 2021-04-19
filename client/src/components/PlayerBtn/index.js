b296a8ffb999f4eaf6d47fa23c84a062fccca8ef
Rachel Wanke <rkhorne09@gmail.com>
2021-04-19 01:32:49 -0400

    adding files for music player

--- client/src/components/PlayerBtn/index.js
+++ client/src/components/PlayerBtn/index.js
@@ -3,76 +3,85 @@ import flower from "./flowerwater.png"
 import MusicPlayer from "../MusicPlayer"
 import * as $ from "jquery";
 import {Button} from "react-bootstrap"
-
-
-
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
-// Replace with your app's client ID, redirect URI and desired scopes
-const clientId = "f2424679bfac43698fa578f4f35ad2e0";
-const redirectUri = "http://localhost:3000";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
-// Get the hash of the url
-const hash = window.location.hash
-  .substring(1)
-  .split("&")
-  .reduce(function(initial, item) {
-    if (item) {
-      var parts = item.split("=");
-      initial[parts[0]] = decodeURIComponent(parts[1]);
-    }
-    return initial;
-  }, {});
-  
-window.location.hash = "";
+import hash from "./hash";
+import { authEndpoint, clientId, redirectUri, scopes } from "./configID";
 
 class PlayerBtn extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+      item: {
+        album: {
+          images: [{ url: "" }]
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms: 0
+      },
+      is_playing: "Paused",
+      progress_ms: 0,
+      no_data: false,
+    };
+
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+    this.tick = this.tick.bind(this);
+  }
+
 
 
   componentDidMount() {
     // Set token
     let _token = hash.access_token;
+
     if (_token) {
       // Set token
       this.setState({
         token: _token
       });
+      this.getCurrentlyPlaying(_token);
+    }
+
+    // set interval for polling every 5 seconds
+    this.interval = setInterval(() => this.tick(), 5000);
+  }
+
+  componentWillUnmount() {
+    // clear the interval to save resources
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    if(this.state.token) {
+      this.getCurrentlyPlaying(this.state.token);
     }
   }
- 
-  constructor() {
-    super();
-    this.state = {
-      token: null,
-    item: {
-      album: {
-        images: [{ url: "" }]
-      },
-      name: "",
-      artists: [{ name: "" }],
-      duration_ms:0,
-    },
-    is_playing: "Paused",
-    progress_ms: 0
-  };
-  this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
-  }
-  
+
+
   getCurrentlyPlaying(token) {
     // Make a call using the token
     $.ajax({
       url: "https://api.spotify.com/v1/me/player",
       type: "GET",
-      beforeSend: (xhr) => {
+      beforeSend: xhr => {
         xhr.setRequestHeader("Authorization", "Bearer " + token);
       },
-      success: (data) => {
+      success: data => {
+        // Checks if the data is not empty
+        if(!data) {
+          this.setState({
+            no_data: true,
+          });
+          return;
+        }
+
         this.setState({
           item: data.item,
           is_playing: data.is_playing,
           progress_ms: data.progress_ms,
+          no_data: false /* We need to "reset" the boolean, in case the
+                            user does not give F5 and has opened his Spotify. */
         });
       }
     });
@@ -85,12 +94,12 @@ render (){
       <header className="App-header">
       <img src={flower} className="App-logo" alt="flower" />
       {!this.state.token && (
-        <Button
+        <a
           className="btn btn--loginApp-link"
           href={`${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
         >
           Login to Spotify
-        </Button>
+        </a>
       )}
       {this.state.token && (
           <MusicPlayer
