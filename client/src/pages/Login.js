c8090de95902d2c8d97a503b0ed75ff958954fdd
Young Ji Kim <youjmik@gmail.com>
2021-04-20 11:46:59 -0400

    working on Log in Sign up

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,6 @@
 
-import React from "react"
-import { Form, Button, Card, Alert } from "react-bootstrap"
+import React, {useState} from "react"
+import { Form, FormControl, InputGroup, Button, Card, Alert } from "react-bootstrap"
 // import { useAuth } from "../contexts/AuthContext"
 // import Hero from "../components/Hero";
 // import Container from "../components/Container";
@@ -10,12 +10,31 @@ import { Form, Button, Card, Alert } from "react-bootstrap"
 // import { Card } from "reactstrap";
 import { Link } from "react-router-dom"
 //import 'bootstrap/dist/css/bootstrap.min.css'
+import API from "../utils/user"
 
 
 import "./css/login.css"
 
 //MEDITATION page info..//
 export default function Login () {
+
+
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+
+  const login = () => {
+    API.getUser({
+      username: loginUsername,
+      password: loginPassword
+    
+    })
+      .then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      });
+
+  }
 
   
   return (
@@ -24,22 +43,46 @@ export default function Login () {
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Log In</h2>
-          {/* {error && <Alert variant="danger">{error}</Alert>} */}
-          <Form > {/*onSubmit={handleSubmit}*/}
-            <Form.Group id="email">
+          {/* <Form >  */}
+            {/* <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" required /> {/*ref={} */}
+              <Form.Control type="email" required /> 
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" required />{/*ref={} */}
-            </Form.Group>
+              <Form.Control type="password" required />
+            </Form.Group> */}
+         <div className ="container">
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="email"
+                aria-label="Session Name"
+                aria-describedby="basic-addon2"
+                type= "email"
+                onChange={e => setLoginUsername(e.target.value)}
+              // value={isName}
+
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="password"
+                aria-label="Session Name"
+                aria-describedby="basic-addon2"
+                type ="password"
+                onChange={e => setLoginPassword(e.target.value)}
+              // value={isName}
+
+              />
+            </InputGroup>
+          </div>
+
             <div>
-            <button  className="w-10 mt-3" type="submit">  {/*disabled={loading}*/}
+            <button  className="w-10 mt-3" type="submit" onClick={login}>  
               Log In
             </button>
             </div>
-          </Form>
+          {/* </Form> */}
           {/* <div className="w-100 text-center mt-3">
             <Link to="/forgot-password">Forgot Password?</Link>
           </div> */}
--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,64 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/signup.css"
-import { Form, Button, Card, Alert } from "react-bootstrap"
+import { FormControl, InputGroup, Button, Card, Alert } from "react-bootstrap"
 // import { useAuth } from "../contexts/AuthContext"
 import { Link } from "react-router-dom"
 //import 'bootstrap/dist/css/bootstrap.min.css'
+import API from "../utils/user"
 
 
-export default function Signup () {
-  // const emailRef = useRef()
-  // const passwordRef = useRef()
-  // const passwordConfirmRef = useRef()
-  // const { signup } = useAuth()
-  // const [error, setError] = useState("")
-  // const [loading, setLoading] = useState(false)
-  // const history = useHistory()
 
-  // async function handleSubmit(e) {
-  //   e.preventDefault()
 
-  //   if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-  //     return setError("Passwords do not match")
-  //   }
+export default function Signup() {
 
-  //   try {
-  //     setError("")
-  //     setLoading(true)
-  //     await signup(emailRef.current.value, passwordRef.current.value)
-  //     history.push("/")
-  //   } catch {
-  //     setError("Failed to create an account")
-  //   }
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  const [data, setData] = useState(null);
 
-  //   setLoading(false)
-  // }
+  const register = () => {
+
+    API.saveUser({
+ 
+      username: registerUsername,
+      password: registerPassword
+    
+    })
+      .then((res) => {
+        console.log(res)
+      }).catch((err) => {
+        console.log(err)
+      });
+
+  }
 
   return (
-    <div className ="container signupboxForm">
+    <div className="container signupboxForm">
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Sign Up</h2>
           {/* {error && <Alert variant="danger">{error}</Alert>} */}
-          <Form > {/*onSubmit={handleSubmit}*/}
+          {/* <Form > 
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email"  required />{/*ref={} */}
+              <Form.Control type="email" onChange={(e) => setRegisterUsername(e.target.value)} required />
             </Form.Group>
             <Form.Group id="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" required />{/*ref={} */}
+              <Form.Control type="password" required onChange={(e) => setRegisterPassword(e.target.value)} />
             </Form.Group>
             <Form.Group id="password-confirm">
               <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" required /> {/*ref={} */}
-            </Form.Group>
-            <div>
-            <button className="w-10 mt-3" type="submit"> {/*disabled={loading} */}
+              <Form.Control type="password" onChange={(e) => setRegisterPassword(e.target.value)} required />
+            </Form.Group> */}
+          <div>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="email"
+                aria-label="Session Name"
+                aria-describedby="basic-addon2"
+                type= "email"
+                onChange={e => setRegisterUsername(e.target.value)}
+              // value={isName}
+
+              />
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <FormControl
+                placeholder="password"
+                aria-label="Session Name"
+                aria-describedby="basic-addon2"
+                type ="password"
+                onChange={e => setRegisterPassword(e.target.value)}
+              // value={isName}
+
+              />
+            </InputGroup>
+          </div>
+
+
+
+          <div>
+            <button className="w-10 mt-3" type="submit" onClick={register}>
               Sign Up
             </button>
-            </div>
-          </Form>
+          </div>
+          {/* </Form> */}
         </Card.Body>
       </Card>
       <div className="w-100 text-center mt-2 ">
