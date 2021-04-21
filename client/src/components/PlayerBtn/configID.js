61781f7fdcaef0aa8eb338e91331b44cc9a5879f
Rachel Wanke <rkhorne09@gmail.com>
2021-04-21 00:29:54 -0400

    adding css to music player and working on videos

--- client/src/components/PlayerBtn/configID.js
+++
@@ -1,8 +0,0 @@
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
-
-export const clientId = "f2424679bfac43698fa578f4f35ad2e0";
-export const redirectUri = "http://localhost:3000/meditation";
-export const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
