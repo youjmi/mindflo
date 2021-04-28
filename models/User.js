0f0093ceef3bcb8418a8ebc43d46f4ae2a93bdb9
Young Ji Kim <youjmik@gmail.com>
2021-04-28 17:07:51 -0400

    testing

--- models/User.js
+++ models/User.js
@@ -1,79 +1,95 @@
 const crypto = require("crypto")
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs")
-const jwt= require("jsonwebtoken")
+const jwt = require("jsonwebtoken")
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-   username: {type: String, required: [true, "Please Provide User Name"]},
-   email: {type: String, required: [true, "Please Provide an email"], unique: true, 
-   match: [
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    "Please provide a valid email",
+  username: { type: String, required: [true, "Please Provide User Name"] },
+  email: {
+    type: String, required: [true, "Please Provide an email"], unique: true,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+  },
+  blog: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Blog"
+    }
   ],
-},
-//    blog: [
-//     {
-//       type: Schema.Types.ObjectId,
-//       ref: "Blog"
-//     }
-//   ],
-//   dashboard: [
-//    {
-//      type: Schema.Types.ObjectId,
-//      ref: "Dashboard"
-//    }
-//  ],
-  
-  
- password: {
-  type: String,
-  required: [true, "Please add a password"],
-  minlength: 6,
-  select: false,
-},
-resetPasswordToken: String,
-resetPasswordExpire: Date,
+  dashboard: [
+    {
+      selfcare: [{
+        type: {
+          type: String,
+          trim: true,
+        },
+        name: {
+          type: String,
+          trim: true,
+        },
+        duration: {
+          type: Number,
+        },
+        mood: {
+          type: Number,
+        }
+
+      }]
+    }
+  ],
+
+
+  password: {
+    type: String,
+    required: [true, "Please add a password"],
+    minlength: 6,
+    select: false,
+  },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
 });
 
 userSchema.pre("save", async function (next) {
-if (!this.isModified("password")) {
-  next();
-}
+  if (!this.isModified("password")) {
+    next();
+  }
 
-const salt = await bcrypt.genSalt(10);
-this.password = await bcrypt.hash(this.password, salt);
-next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
 });
 
 userSchema.methods.matchPassword = async function (password) {
-return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 userSchema.methods.getSignedJwtToken = function () {
-return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-  expiresIn: process.env.JWT_EXPIRE, 
-});
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
 };
 
 userSchema.methods.getResetPasswordToken = function () {
-const resetToken = crypto.randomBytes(20).toString("hex");
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-// // Hash token (private key) and save to database
-this.resetPasswordToken = crypto
-  .createHash("sha256")
-  .update(resetToken)
-  .digest("hex");
- 
-// // Set token expire date
-this.resetPasswordExpire = Date.now() + 10 * (60 * 1000); // Ten Minutes
+  // // Hash token (private key) and save to database
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
 
-return resetToken;
+  // // Set token expire date
+  this.resetPasswordExpire = Date.now() + 10 * (60 * 1000); // Ten Minutes
+
+  return resetToken;
 
 };
 
 const User = mongoose.model("User", userSchema);
 
-module.exports = User; 
+module.exports = User;
 
